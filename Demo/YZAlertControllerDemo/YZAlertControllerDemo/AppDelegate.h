1ee19b340595456facade7416ee2d5e5f37a3059
叶志强 <443023263@qq.com>
2018-07-23 19:05:49 +0800

    Adjust the directory structure

---
+++ Demo/YZAlertControllerDemo/YZAlertControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZAlertControllerDemo
+//
+//  Created by 叶志强 on 2018/7/23.
+//  Copyright © 2018年 CancerQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/YZAlertControllerDemo/YZAlertControllerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YZAlertControllerDemo
+//
+//  Created by 叶志强 on 2018/7/23.
+//  Copyright © 2018年 CancerQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
